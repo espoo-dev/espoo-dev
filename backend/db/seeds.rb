57c9bd4698daa64fe61d57c3124a4440de597fd4
Edimo Sousa <edimossilva@gmail.com>
2021-04-07 22:11:34 +0300

    add seed

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create email: 'admin@gmail.com', password: '123456', role: User::ROLES[0]
