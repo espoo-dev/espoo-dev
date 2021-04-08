4213ccb83de6b5a9e4dca92af1f30b324e94c9bd
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-04-08 17:23:18 -0300

    Updating _app

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,13 +1,20 @@
-import "../styles/globals.scss";
-import { ChakraProvider } from "@chakra-ui/react";
-import Navbar from "../components/Navbar/Navbar";
+import Head from "next/head";
+import { GlobalStyle } from "../styles/globals";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Navbar />
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <GlobalStyle />
+
       <Component {...pageProps} />
-    </ChakraProvider>
+    </>
   );
 }
 
