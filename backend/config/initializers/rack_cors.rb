5d524a1e121af28410b4b6b8e3ba94597efd5f80
Edimo Sousa <edimossilva@gmail.com>
2021-04-16 13:56:37 +0300

    expose Authorization header

--- backend/config/initializers/rack_cors.rb
+++ backend/config/initializers/rack_cors.rb
@@ -3,6 +3,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins '*'
     resource '*',
 		headers: :any,
+    expose: ['Authorization'],
 		methods: [:get, :post, :patch, :put, :options]
   end
 end
