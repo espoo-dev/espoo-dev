9f6ed7de70e9e1d6cecc9cf7d7c3be9227126365
Anderson Malhero de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-04-23 19:14:48 -0300

    Adding register user function on auth context and improving checkToken function

--- frontend/src/context/auth.tsx
+++ frontend/src/context/auth.tsx
@@ -1,22 +1,19 @@
-import React, {
-  createContext,
-  useCallback,
-  useEffect,
-  useState,
-} from 'react';
+import React, { createContext, useCallback, useState } from 'react';
 import { useRouter } from 'next/router';
 import { errorHandler } from 'api/error-handler';
 import { httpClient } from 'api';
 import { AuthService } from 'api/services';
-import { User, UserLogin } from 'api/models/user';
+import { User, UserCreate, UserLogin } from 'api/models/user';
 import { toast } from 'react-toastify';
 
 export interface AuthContextProps {
   user: User | null;
   isAuthenticated: boolean;
   loading: boolean;
+  checkToken: () => Promise<boolean>;
   login: (data: UserLogin) => void;
   logout: () => void;
+  register: (data: UserCreate) => void;
 }
 
 export const AuthContext = createContext<AuthContextProps>(
@@ -28,7 +25,7 @@ const authService = new AuthService(httpClient);
 export const AuthProvider: React.FC = ({ children }) => {
   const [user, setUser] = useState<User | null>(null);
   const [token, setToken] = useState<string>(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   const login = useCallback(async (data: UserLogin) => {
@@ -58,7 +55,29 @@ export const AuthProvider: React.FC = ({ children }) => {
       setLoading(false);
       errorHandler(error);
     }
-  }, [router]);
+  }, []);
+
+  const register = useCallback(async (data: UserCreate) => {
+    try {
+      setLoading(true);
+      const response = await authService.register(data);
+
+      if (response) {
+        toast('Successfully registered, now you can log in', {
+          position: 'top-right',
+          type: 'success',
+          pauseOnHover: false,
+        });
+
+        router.replace('/login');
+
+        setLoading(false);
+      }
+    } catch (error) {
+      setLoading(false);
+      errorHandler(error);
+    }
+  }, []);
 
   const logout = useCallback(() => {
     localStorage.removeItem('token');
@@ -66,26 +85,33 @@ export const AuthProvider: React.FC = ({ children }) => {
     setUser(null);
   }, []);
 
-  const checkToken = useCallback(() => {
-    setLoading(true);
+  const checkToken = async () => {
+    const checking = new Promise<boolean>((resolve) => {
+      const storedToken = localStorage.getItem('token');
 
-    const storedToken = localStorage.getItem('token');
-    setLoading(false);
+      if (!storedToken) {
+        router.replace('/login');
+        resolve(true);
+      } else {
+        setToken(storedToken);
+        resolve(false);
+      }
+    });
 
-    if (!storedToken) {
-      router.replace('/login');
-    } else {
-      setToken(storedToken);
-    }
-  }, [router, setToken]);
-
-  useEffect(() => {
-    checkToken();
-  }, [checkToken]);
+    return checking;
+  };
 
   return (
     <AuthContext.Provider
-      value={{ isAuthenticated: !!token, user, login, loading, logout }}
+      value={{
+        checkToken,
+        isAuthenticated: !!token,
+        user,
+        login,
+        loading,
+        logout,
+        register,
+      }}
     >
       {children}
     </AuthContext.Provider>
