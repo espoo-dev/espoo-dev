458e69942d3c8d0805d4c1b70ac7b70e58199101
Anderson Malheiro de Carvalho <41576677+andersonmalheiro@users.noreply.github.com>
2021-04-26 18:18:25 -0300

    develop -> main (#71)
    * [feature/frontend-project] - Frontend initial commit
    
    * [chore/#9] - add chakra ui framework to the project
    
    * [feat/#11] - create responsive navbar
    
    * feature/login (#23)
    
    * Updating folder structure
    
    * Adding input component
    
    * Updating styles with styled-components and creating utils
    
    * Updating _app
    
    * Adding custom _document
    
    * Updating home
    
    * Adding login page structure
    
    * Renaming input component folder
    
    * Adding button component
    
    * Adding api config and integrating login with service
    
    * Refactoring error-handler
    
    * Removing unnused imports and console log
    
    * Adding required validation on login form
    
    * Adding spacing style utils
    
    * Using margin variable from utils on app-input component
    
    * Adding linter config (#40)
    
    * Adding linter config
    
    * Updating eslint rules
    
    * Fixing lint issues (#41)
    
    * Feature - auth context (#33)
    
    * Updating api URL
    
    * Updating auth service return type
    
    * Adding auth context
    
    * Adding withAuth HOC
    
    * Integrating login with auth context
    
    * Fixing babel problem
    
    * Fixing withAuth render problem and properly saving token
    
    * Removing unused code
    
    * register user page (#53)
    
    * Adding register user model and endpoint on api
    
    * Adding register user function on auth context and improving checkToken function
    
    * Fixing render issues on withAuth HOC
    
    * Adding register page
    
    * Adding link on login to register page
    
    * fixing linter issues
    
    Co-authored-by: Natali <nanatkim11@gmail.com>

--- frontend/src/api/models/user.ts
+++ frontend/src/api/models/user.ts
@@ -3,8 +3,17 @@ export interface UserLogin {
   password: string;
 }
 
+type Role = 'teacher' | 'moderator';
+
 export interface User {
   email: string;
   id: number;
   phone: string;
+  role: Role;
+}
+
+export interface UserCreate {
+  email: string;
+  password: string;
+  role: Role;
 }
---
+++ frontend/src/context/auth.tsx
@@ -0,0 +1,119 @@
+import React, { createContext, useCallback, useState } from 'react';
+import { useRouter } from 'next/router';
+import { errorHandler } from 'api/error-handler';
+import { httpClient } from 'api';
+import { AuthService } from 'api/services';
+import { User, UserCreate, UserLogin } from 'api/models/user';
+import { toast } from 'react-toastify';
+
+export interface AuthContextProps {
+  user: User | null;
+  isAuthenticated: boolean;
+  loading: boolean;
+  checkToken: () => Promise<boolean>;
+  login: (data: UserLogin) => void;
+  logout: () => void;
+  register: (data: UserCreate) => void;
+}
+
+export const AuthContext = createContext<AuthContextProps>(
+  {} as AuthContextProps
+);
+
+const authService = new AuthService(httpClient);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [token, setToken] = useState<string>(null);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const login = useCallback(async (data: UserLogin) => {
+    try {
+      setLoading(true);
+      const response = await authService.authenticate(data);
+
+      if (response) {
+        const { authorization } = response.headers;
+
+        toast(`Welcome ${response.data.email}`, {
+          position: 'top-right',
+          type: 'success',
+          pauseOnHover: false,
+        });
+
+        router.replace('/main');
+
+        httpClient.defaults.headers.Authorization = authorization;
+
+        setLoading(false);
+        setUser(response.data);
+        setToken(authorization);
+        localStorage.setItem('token', JSON.stringify(authorization));
+      }
+    } catch (error) {
+      setLoading(false);
+      errorHandler(error);
+    }
+  }, []);
+
+  const register = useCallback(async (data: UserCreate) => {
+    try {
+      setLoading(true);
+      const response = await authService.register(data);
+
+      if (response) {
+        toast('Successfully registered, now you can log in', {
+          position: 'top-right',
+          type: 'success',
+          pauseOnHover: false,
+        });
+
+        router.replace('/login');
+
+        setLoading(false);
+      }
+    } catch (error) {
+      setLoading(false);
+      errorHandler(error);
+    }
+  }, []);
+
+  const logout = useCallback(() => {
+    localStorage.removeItem('token');
+    setToken(null);
+    setUser(null);
+  }, []);
+
+  const checkToken = async () => {
+    const checking = new Promise<boolean>((resolve) => {
+      const storedToken = localStorage.getItem('token');
+
+      if (!storedToken) {
+        router.replace('/login');
+        resolve(true);
+      } else {
+        setToken(storedToken);
+        resolve(false);
+      }
+    });
+
+    return checking;
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        checkToken,
+        isAuthenticated: !!token,
+        user,
+        login,
+        loading,
+        logout,
+        register,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
