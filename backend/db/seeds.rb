28cc7dc2a49f6c1273dce6965658030f6e87cdd1
Edimo Sousa <edimossilva@gmail.com>
2021-05-01 09:44:18 +0300

    improve db usage

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,11 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-FactoryBot.create(:role_admin)
-FactoryBot.create(:survey)
-FactoryBot.create(:question)
-FactoryBot.create(:user, email: 'admin@gmail.com')
+role = Role.create(role_type: 'admin')
+admin = User.create(email: 'admin@gmail.com', password: '123456', role: role)
