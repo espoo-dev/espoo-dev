88450bd7a77f7bbd1d10fa950956db2f1c98a662
garciajordy <jordygarcia0119@gmail.com>
2021-05-05 17:17:34 +0300

    merge

--- backend/app/models/option.rb
+++ backend/app/models/option.rb
@@ -1,4 +1,7 @@
 class Option < ApplicationRecord
   belongs_to :question
+<<<<<<< HEAD
   validates :option_type, presence: true
+=======
+>>>>>>> 2aab2dc (Add option model and updated ERD)
 end
--- backend/spec/factories/options.rb
+++ backend/spec/factories/options.rb
@@ -1,6 +1,11 @@
 FactoryBot.define do
   factory :option do
+<<<<<<< HEAD
     option_type { 'option 1' }
     association :question
+=======
+    option_type { "MyString" }
+    question { nil }
+>>>>>>> 2aab2dc (Add option model and updated ERD)
   end
 end
