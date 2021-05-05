5676081ebb5edb43aa646bf0320c89191fa8dfe8
garciajordy <jordygarcia0119@gmail.com>
2021-05-05 17:17:34 +0300

    merge

--- backend/spec/models/option_spec.rb
+++ backend/spec/models/option_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Option, type: :model do
+<<<<<<< HEAD
   let(:option) { create(:option) }
 
   it { expect(option).to be_valid }
@@ -8,4 +9,7 @@ RSpec.describe Option, type: :model do
   describe 'presence' do
     it { is_expected.to validate_presence_of(:option_type) }
   end
+=======
+  pending "add some examples to (or delete) #{__FILE__}"
+>>>>>>> 2aab2dc (Add option model and updated ERD)
 end
