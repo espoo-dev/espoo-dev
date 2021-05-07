6d3d9e27d70cc5af971c9bc73c0944fcfad640d6
garciajordy <jordygarcia0119@gmail.com>
2021-05-07 21:35:48 +0300

    Solve Git Issue

--- backend/spec/models/role_spec.rb
+++ backend/spec/models/role_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe Role, type: :model do
   describe 'presence' do
     it { is_expected.to validate_presence_of(:role_type) }
   end
+
+  describe 'uniqueness' do
+    it { is_expected.to validate_uniqueness_of(:role_type) }
+  end
 end
