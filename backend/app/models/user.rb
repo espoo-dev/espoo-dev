50d0a13c1e47dcb1da91a8562eafb1dc86f8c846
alexvko <ale@alexvko.com>
2021-05-09 15:53:56 +0000

    Useless commit

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
 
-  has_many :surveys, dependent: :destroy
-  has_many :questions, dependent: :destroy
   belongs_to :role
+  has_many :questions, dependent: :destroy
+  has_many :surveys, dependent: :destroy
 
   validates :role, presence: true
   delegate :admin?, to: :role
