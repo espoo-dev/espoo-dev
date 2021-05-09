1525d42decc4f52c11b08384dc81f48529ac6834
Edimo Sousa <edimossilva@gmail.com>
2021-05-09 09:46:10 +0300

    Add option policy

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_05_07_070142) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
