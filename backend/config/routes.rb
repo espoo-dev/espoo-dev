ccee6b0d71b90ebf133cb3d82831a9bb1e7356e0
garciajordy <jordygarcia0119@gmail.com>
2021-05-12 20:14:02 +0300

    Add endpoint to list surveys by user id
    Co-authored-by: Daniel Nottingham <danielnottingham@users.noreply.github.com>

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: %i[index create]
-      resources :surveys, only: %i[show]
+      resources :surveys, only: %i[index show]
       resources :roles, only: %i[index]
     end
   end
