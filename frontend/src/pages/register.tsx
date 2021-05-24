be0599dc83faf6df7dacafcb5f33d328d26e3261
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-05-24 20:28:54 -0300

    Refactoring role field on register to user options from server

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,9 +1,13 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { errorHandler } from 'api/error-handler';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { AppButton } from 'components/app-button';
 import { AuthContext } from 'context/auth';
+import { Role } from 'api/models/role';
 import { UserCreate } from 'api/models/user';
+import { RoleService } from 'api/services';
+import { httpClient } from 'api';
 import { FlexColumn, FlexRow } from '../styles/utils';
 import {
   Container,
@@ -13,21 +17,44 @@ import {
   MainCard,
   MainImg,
 } from '../styles/register.styles';
-import { AppInput } from '../components';
+import { AppInput, AppSelect } from '../components';
 
 const Login = () => {
   const formRef = useRef();
   const context = useContext(AuthContext);
   const { register, loading } = context;
   const router = useRouter();
+  const [roles, setRoles] = useState<Role[]>([]);
+  const roleService = new RoleService(httpClient);
 
   const handleFormSubmit = (data: UserCreate) => {
-    register(data);
+    const { role_id } = data;
+    const payload: UserCreate = {
+      ...data,
+      role_id: Number(role_id),
+    };
+
+    register(payload);
   };
 
   const goToLogin = () => {
     router.replace('/login');
   };
+
+  const getRoles = async () => {
+    try {
+      const response = await roleService.list();
+      if (response && response.data) {
+        setRoles(response.data);
+      }
+    } catch (error) {
+      errorHandler(error);
+    }
+  };
+
+  useEffect(() => {
+    getRoles();
+  }, []);
 
   return (
     <Container>
@@ -68,14 +95,20 @@ const Login = () => {
             type="password"
             required
           />
-          <AppInput
-            id="role"
-            name="role"
+          <AppSelect
+            id="role_id"
+            name="role_id"
             label="Role"
             placeholder="teacher, moderator..."
-            type="text"
             required
-          />
+          >
+            <option value="">Select a role</option>
+            {roles.map((role) => (
+              <option key={role.id} value={role.id}>
+                {role.role_type}
+              </option>
+            ))}
+          </AppSelect>
 
           <AppButton
             styling="primary"
