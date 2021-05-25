a02da5d1041ed3469bfa0603e6d84f4a51bf2399
Iury Nogueira <iunog@hotmail.com>
2021-05-25 20:41:07 -0300

    :hammer: Refactor and improve tests with cy interceptor #151

--- frontend/cypress/integration/login/login.spec.js
+++ frontend/cypress/integration/login/login.spec.js
@@ -17,10 +17,13 @@ context('Login test', () => {
 
   it('should show and type password', () => {
     cy.get('#password')
-      .type('123456');
+      .type('123456')
+      .should('have.value', '123456');
   });
 
   it('try login with invalid user', () => {
+    cy.intercept('https://espoo.herokuapp.com/users/sign_in*').as('sign_in');
+
     cy.get('#email')
       .type('invaliduser@gmail.com');
 
@@ -30,10 +33,16 @@ context('Login test', () => {
     cy.get('#btn-login')
       .click();
 
-    cy.url().should('not.be.include', '/main');
+    cy.wait('@sign_in').then(
+      (res, _) => {
+        cy.expect(res.response.statusCode).to.be.eql(401);
+      }
+    );
   });
 
   it('should do login and check has logout button', () => {
+    cy.intercept('https://espoo.herokuapp.com/users/sign_in*').as('sign_in');
+
     cy.get('#email')
       .type('admin@gmail.com');
 
@@ -43,6 +52,10 @@ context('Login test', () => {
     cy.get('#btn-login')
       .click();
 
-    cy.url().should('include', '/main');
+    cy.wait('@sign_in').then(
+      (res, _) => {
+        cy.expect(res.response.statusCode).to.be.eql(200);
+      }
+    );
   });
 });
