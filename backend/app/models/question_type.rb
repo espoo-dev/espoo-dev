e5f5bd6de49836db41a0b9bc4222de39115cb156
garciajordy <jordygarcia0119@gmail.com>
2021-05-27 14:15:26 +0300

    Add correct column to options

--- backend/app/models/question_type.rb
+++ backend/app/models/question_type.rb
@@ -2,4 +2,7 @@ class QuestionType < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false }
   has_many :questions, dependent: :destroy
+
+  SINGLE_CHOICE = 'Single Choice'.freeze
+  MULTIPLE_CHOICE = 'Multiple Choice'.freeze
 end
