18c656450951309a2e7dc219a4d988746290f995
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-05-31 19:49:04 -0300

    Adding teacher list test

---
+++ frontend/src/components/main/teacher-list.test.tsx
@@ -0,0 +1,67 @@
+import { User } from 'api/models/user';
+import React from 'react';
+import { fireEvent, render, screen } from 'test-utils';
+import { TeachersList } from './TeachersList';
+
+describe('Teacher list', () => {
+  it('should not render the component without the props', () => {
+    expect(() => render(<TeachersList />)).toThrow();
+  });
+
+  it('should render the item passed to the list', () => {
+    const data: User[] = [
+      {
+        email: 'admin@gmail.com',
+        id: 1,
+        phone: '',
+        role: {
+          id: 1,
+          role_type: 'admin',
+        },
+      },
+    ];
+
+    const { getByText } = render(
+      <TeachersList data={data} onSelect={() => {}} />
+    );
+
+    const user = getByText('admin@gmail.com');
+
+    expect(user).toBeInTheDocument();
+  });
+
+  it('should call the function when clicked', () => {
+    const mockSelect = jest.fn();
+    const data: User[] = [
+      {
+        email: 'admin@gmail.com',
+        id: 1,
+        phone: '',
+        role: {
+          id: 1,
+          role_type: 'admin',
+        },
+      },
+    ];
+
+    const { getByTestId } = render(
+      <TeachersList data={data} onSelect={mockSelect} />
+    );
+
+    const listItem = getByTestId('admin@gmail.com');
+
+    fireEvent.click(listItem);
+
+    expect(mockSelect).toBeCalledTimes(1);
+  });
+
+  it('should not render the component without the props', () => {
+    const { getByText } = render(
+      <TeachersList data={[]} onSelect={() => {}} />
+    );
+
+    const emptyMsg = getByText('No data to display...');
+
+    expect(emptyMsg).toBeInTheDocument();
+  });
+});
