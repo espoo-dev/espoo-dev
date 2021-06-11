47442453f700e9e3f67cf25a2e608855bb30de52
garciajordy <jordygarcia0119@gmail.com>
2021-06-11 13:46:30 +0300

    Add Sentry key to ENV

--- backend/app/controllers/admin/questions_controller.rb
+++ backend/app/controllers/admin/questions_controller.rb
@@ -2,7 +2,6 @@ module Admin
   class QuestionsController < Admin::ApplicationController
     def new_resource
       resource_class.new(user: current_user)
-      raise 'Jordy Is the best'
     end
   end
 end
--- backend/config/initializers/sentry.rb
+++ backend/config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-    config.dsn = 'https://b9f58db50cc345c6bc2a34ab467d13ac@o828359.ingest.sentry.io/5811570'
+    config.dsn = ENV["SENTRY_KEY"]
     config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   
     # Set tracesSampleRate to 1.0 to capture 100%
