ed36414ec1993aaa6699fb174dfb6fcb59c2deb5
garciajordy <jordygarcia0119@gmail.com>
2021-06-22 09:16:35 +0300

    Add SurveySubject model

--- backend/spec/models/survey_spec.rb
+++ backend/spec/models/survey_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Survey, type: :model do
   describe 'relationships' do
     it { is_expected.to belong_to(:user).required }
+    it { is_expected.to belong_to(:survey_subject).required }
     it { is_expected.to have_many(:questions).dependent(:nullify) }
     it { is_expected.to have_many(:answers_surveys).dependent(:destroy) }
   end
