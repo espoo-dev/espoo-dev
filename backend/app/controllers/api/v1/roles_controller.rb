009c55bb52c9f3455bbe473dc88bd47d5cd7fb56
garciajordy <jordygarcia0119@gmail.com>
2021-06-24 18:28:11 +0300

    Only show all roles when admin in roles endpoint

--- backend/app/controllers/api/v1/roles_controller.rb
+++ backend/app/controllers/api/v1/roles_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::RolesController < Api::V1::ApiController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    render json: Role.all
+    render json: Role.by_user(current_user)
   end
 end
