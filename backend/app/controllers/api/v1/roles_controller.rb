c1a7a9a24678fb3522fd91c619990f2af7fd1d04
garciajordy <jordygarcia0119@gmail.com>
2021-06-24 18:28:11 +0300

    Only show all roles when admin in roles endpoint

--- backend/app/controllers/api/v1/roles_controller.rb
+++ backend/app/controllers/api/v1/roles_controller.rb
@@ -2,6 +2,12 @@ class Api::V1::RolesController < Api::V1::ApiController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    render json: Role.all
+    roles = if current_user&.admin?
+              Role.all
+            else
+              Role.where.not(role_type: Role::ADMIN)
+            end
+
+    render json: roles
   end
 end
--- backend/spec/requests/roles_request_spec.rb
+++ backend/spec/requests/roles_request_spec.rb
@@ -2,14 +2,40 @@ require 'rails_helper'
 
 RSpec.describe 'RolesController', type: :request do
   describe '#list' do
-    let!(:role) { create(:role) }
+    let!(:role_moderator) { create(:role_moderator) }
+    let!(:role_admin) { create(:role_admin) }
+    let!(:role_teacher) { create(:role_teacher) }
+    let!(:role_student) { create(:role_student) }
+    let(:user_admin) { create(:user) }
 
-    before { get api_v1_roles_path }
+    describe 'List all roles when admin' do
+      before do
+        get api_v1_roles_path, headers: auth_headers(user: user_admin)
+      end
 
-    it { expect(response).to have_http_status :ok }
+      it { expect(response).to have_http_status :ok }
 
-    it 'matches surveys attributes' do
-      expect(response_body).to match([{ 'id' => role.id, 'role_type' => role.role_type }])
+      it 'matches surveys attributes' do
+        expect(response_body).to match([{ 'id' => role_moderator.id, 'role_type' => role_moderator.role_type }, { 'id' => role_admin.id, 'role_type' => role_admin.role_type },
+                                        { 'id' => role_teacher.id, 'role_type' => role_teacher.role_type }, { 'id' => role_student.id, 'role_type' => role_student.role_type }])
+      end
+    end
+
+    describe 'List all roles except admin when not logged in' do
+      before do
+        get api_v1_roles_path
+      end
+
+      it { expect(response).to have_http_status :ok }
+
+      it 'matches surveys attributes' do
+        expect(response_body).to match([{ 'id' => role_moderator.id, 'role_type' => role_moderator.role_type },
+                                        { 'id' => role_teacher.id, 'role_type' => role_teacher.role_type }, { 'id' => role_student.id, 'role_type' => role_student.role_type }])
+      end
+
+      it 'does not show admin role' do
+        expect(response_body).not_to include({ 'id' => role_admin.id, 'role_type' => role_admin.role_type })
+      end
     end
   end
 end
