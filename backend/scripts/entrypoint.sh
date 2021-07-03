a3cb42142a45f84e4d97d6aab7fa04724460b412
Edimo Sousa <edimossilva@gmail.com>
2021-07-03 18:17:24 +0300

    docker -  fix sh paths

--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 bundle install
 
+#config env to not need to use bundle exec before commands
+mkdir -p .git/safe
+ENV PATH=".git/safe/../../bin:$PATH"
+export PATH=".git/safe/../../bin:$PATH"
+
 rake db:exists && rake db:migrate || rake db:setup
 
 rm /app/tmp/pids/*
