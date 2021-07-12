a15547ff55b57f0bb3903e27859f8066d59415cd
Anderson Malheiro de Carvalho <41576677+andersonmalheiro@users.noreply.github.com>
2021-07-12 20:49:50 -0300

    Feature/storybook setup (#294)
    * [feature/storybook-setup] - Updating paths on jest and tsconfig
    
    * [feature/storybook-setup] - adding storybook dependencies
    
    * [feature/storybook-setup] - adding storybook config and initial example
    
    * [feature/storybook-setup] - adding button example and updating imports

---
+++ frontend/.storybook/main.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const toPath = (_path) => path.join(process.cwd(), _path);
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+  ],
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@emotion/core': toPath('node_modules/@emotion/react'),
+      'emotion-theming': toPath('node_modules/@emotion/react'),
+      '@components': path.resolve(__dirname, '../src/components'),
+      '@pages': path.resolve(__dirname, '../src/pages'),
+      '@hooks': path.resolve(__dirname, '../src/hooks'),
+      '@styles': path.resolve(__dirname, '../src/styles'),
+    };
+
+    return config;
+  },
+};
