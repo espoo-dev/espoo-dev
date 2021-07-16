a1af548259dcb1604355252d186446ec96d15b20
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-07-15 21:59:30 -0300

    adding new color to colors object

--- frontend/src/styles/colors.ts
+++ frontend/src/styles/colors.ts
@@ -1,5 +1,6 @@
 export const colors: Record<string, string> = {
   primary: 'linear-gradient(90deg, #62b6b0 0%,#489fa3 100%)',
+  primary_simple: '#62b6b0',
   secondary: '#ffffff',
   success: '#4caf50',
   danger: '#f44336',
