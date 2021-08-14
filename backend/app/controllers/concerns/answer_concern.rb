dfc45dcc7a24ab96fadb3f984c5b5cde684bfff1
Edimo Silva <edimossilva@gmail.com>
2021-08-14 09:59:49 +0300

    support free_text answer creation

--- backend/app/controllers/concerns/answer_concern.rb
+++ backend/app/controllers/concerns/answer_concern.rb
@@ -3,8 +3,10 @@ module AnswerConcern
 
   def build_answer(answer_params, option_ids)
     answer = Answer.new(answer_params)
-    options = Option.includes(:user, :question).find(option_ids)
-    answer.options = options
+    if !answer.free_text?
+      options = Option.includes(:user, :question).find(option_ids)
+      answer.options = options
+    end
     answer
   end
 end
