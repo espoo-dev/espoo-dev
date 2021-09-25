e4eaca6187709ebe9f7a4202201c587abffa3e3b
Edimo Silva <edimossilva@gmail.com>
2021-09-25 11:52:10 +0300

    Add status to AnswersSurvey Json (#314)
    * Add status to answer survey
    
    * Add status to Answer Survey Json
    
    * Disable FeatureEnvy on reek

--- backend/spec/models/answers_survey_spec.rb
+++ backend/spec/models/answers_survey_spec.rb
@@ -6,4 +6,30 @@ RSpec.describe AnswersSurvey, type: :model do
     it { is_expected.to belong_to(:survey).required }
     it { is_expected.to have_many(:answers).dependent(:destroy) }
   end
+
+  describe '#status' do
+    describe 'when has no answers' do
+      let(:answer_survey) { create(:answers_survey, answers: []) }
+
+      it 'has status NOT_STARTED' do
+        expect(answer_survey.status).to eq(AnswersSurvey::NOT_STARTED)
+      end
+    end
+
+    describe 'when has some answers' do
+      let(:answer_survey) { create(:answers_survey_with_some_answers) }
+
+      it 'has status STARTED' do
+        expect(answer_survey.status).to eq(AnswersSurvey::STARTED)
+      end
+    end
+
+    describe 'when has all answers' do
+      let(:answer_survey) { create(:answers_survey_with_all_answers) }
+
+      it 'has status COMPLETED' do
+        expect(answer_survey.status).to eq(AnswersSurvey::COMPLETED)
+      end
+    end
+  end
 end
