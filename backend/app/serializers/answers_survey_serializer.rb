ee03c48052da1d5ad3f974c1d62da3734eb94998
Edimo Silva <edimossilva@gmail.com>
2021-09-25 11:33:53 +0300

    Add status to Answer Survey Json

--- backend/app/serializers/answers_survey_serializer.rb
+++ backend/app/serializers/answers_survey_serializer.rb
@@ -1,5 +1,5 @@
 class AnswersSurveySerializer < ActiveModel::Serializer
-  attributes :id, :survey, :user_id
+  attributes :id, :survey, :user_id, :status
 
   def survey
     SurveySerializer.new(object.survey)
