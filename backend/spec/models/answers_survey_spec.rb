ff17de7832e5ff90107c3c141cee37a465993b92
Edimo Silva <edimossilva@gmail.com>
2021-09-25 13:10:36 +0300

    Add validations to answer survey Only one answer survey can be not completed by survey and user

--- backend/spec/models/answers_survey_spec.rb
+++ backend/spec/models/answers_survey_spec.rb
@@ -32,4 +32,33 @@ RSpec.describe AnswersSurvey, type: :model do
       end
     end
   end
+
+  describe '#unique_not_completed_by_survey' do
+    let(:user) { create(:user) }
+    let(:survey) { create(:survey) }
+
+    describe 'when answers_survey is unique "not completed" by survey' do
+      let(:answers_survey) { create(:answers_survey) }
+
+      it 'is valid' do
+        expect(answers_survey).to be_valid
+      end
+    end
+
+    describe 'when answers_survey is not unique "not completed" by survey' do
+      let(:answers_survey1) { create(:answers_survey, user: user, survey: survey) }
+      let(:answers_survey2) { build(:answers_survey, user: user, survey: survey) }
+
+      it 'is not valid' do
+        answers_survey1
+        expect(answers_survey2).not_to be_valid
+      end
+
+      it 'is has multiple_not_completed_answers_survey error' do
+        answers_survey1
+        answers_survey2.valid?
+        expect(answers_survey2.errors.full_messages).to match(['Should exist only one not completed by survey and user.'])
+      end
+    end
+  end
 end
