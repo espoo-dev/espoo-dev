e98cb231fbd254f5b322c1395b564a38816d41b3
Edimo Silva <edimossilva@gmail.com>
2021-09-26 10:30:08 +0300

    Add answer by question and answers_survey

--- backend/app/models/answer.rb
+++ backend/app/models/answer.rb
@@ -6,6 +6,7 @@ class Answer < ApplicationRecord
   has_many :options, through: :answers_options
 
   validates :user_answer, presence: true, if: :free_text?
+  validates :question_id, presence: true, uniqueness: { scope: :answers_survey_id }
 
   delegate :free_text?, to: :question, allow_nil: true
 end
