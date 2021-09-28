8e0a88f82effd33691d201f688743149f39f1f85
garciajordy <jordygarcia0119@gmail.com>
2021-09-28 08:56:46 +0200

    Add option validations for the Answer model
    The `#minimum_one_option` validation is needed for when
    creating an answer without any options this should only be
    available for `free_text` questions.
    
    The `#maximum_one_option` validation is needed when creating
    an answer for a `single_choice` question and selecting more than
    one option.

--- backend/spec/requests/answers_request_spec.rb
+++ backend/spec/requests/answers_request_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'AnswersController', type: :request do
       let!(:user_teacher) { create(:user_teacher) }
       let!(:user_student) { create(:user_student) }
       let!(:answers_survey) { create(:answers_survey, user: user_student) }
-      let!(:question) { create(:question, user: user_teacher) }
+      let!(:question) { create(:multiple_choice_question, user: user_teacher) }
       let!(:option_a) { create(:option, user: user_teacher) }
       let!(:option_b) { create(:option, user: user_teacher) }
 
@@ -78,6 +78,36 @@ RSpec.describe 'AnswersController', type: :request do
           }
           expect(response_body).to match(expected_attributes)
         end
+      end
+    end
+
+    context 'when data is not valid' do
+      let!(:user_teacher) { create(:user_teacher) }
+      let!(:user_student) { create(:user_student) }
+      let!(:answers_survey) { create(:answers_survey, user: user_student) }
+      let!(:question) { create(:question, user: user_teacher) }
+      let!(:option_a) { create(:option, user: user_teacher) }
+      let!(:option_b) { create(:option, user: user_teacher) }
+      let(:option_ids) { [option_a.id, option_b.id] }
+
+      before do
+        answer_params = {
+          question_id: question.id,
+          option_ids: option_ids,
+          answers_survey_id: answers_survey.id
+        }
+
+        post api_v1_answers_path, params: answer_params, headers: auth_headers(user: user_student)
+      end
+
+      context 'when question is single choice and answer has more than one option' do
+        it { expect(response).to have_http_status :unprocessable_entity }
+      end
+
+      context 'when question is not free text and answer has no options' do
+        let(:option_ids) { nil }
+
+        it { expect(response).to have_http_status :unprocessable_entity }
       end
     end
   end
