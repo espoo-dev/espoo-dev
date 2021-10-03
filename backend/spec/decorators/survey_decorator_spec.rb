2e2160d632f1dbc2b72e5e4a7f1fb3f3f417f89c
Edimo Silva <edimossilva@gmail.com>
2021-10-03 10:09:37 +0300

    fix typo

--- backend/spec/decorators/survey_decorator_spec.rb
+++ backend/spec/decorators/survey_decorator_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe SurveyDecorator do
   describe '#title' do
     it 'has singular name when survey has only one question' do
       survey = create(:survey)
-      title = "#{survey.name} (1 question)"
+      title = "#{survey.name} (1 Question)"
       expect(survey.decorate.title).to eq title
     end
 
     it 'has plural name when survey has only one question' do
       survey = create(:survey_with_2_questions)
-      title = "#{survey.name} (2 questions)"
+      title = "#{survey.name} (2 Questions)"
       expect(survey.decorate.title).to eq title
     end
   end
