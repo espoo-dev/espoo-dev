234624549ae45f04364e0da7c81da4f30a0e8751
Edimo Silva <edimossilva@gmail.com>
2021-10-09 11:24:26 +0300

    Create answer survey on surveys page

--- backend/spec/routes_spec.rb
+++ backend/spec/routes_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe 'Routes', type: :routing do
   end
 
   it 'has expected routes amount' do
-    expect(Rails.application.routes.routes.size).to eq(83)
+    expect(Rails.application.routes.routes.size).to eq(84)
   end
 end
