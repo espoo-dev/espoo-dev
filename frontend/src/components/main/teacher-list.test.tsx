027de26eefcc04b3c3837c523e86ea90f3b39f70
Leonardo Brito <f.leobrito@gmail.com>
2021-10-15 19:36:01 -0300

    Remove test that doesn't pass required props

--- frontend/src/components/main/teacher-list.test.tsx
+++ frontend/src/components/main/teacher-list.test.tsx
@@ -1,17 +1,9 @@
 import { User } from 'api/models/user';
 import React from 'react';
-import { fireEvent, render, screen } from 'test-utils';
+import { fireEvent, render } from 'test-utils';
 import { TeachersList } from './TeachersList';
 
 describe('Teacher list', () => {
-  it('should not render the component without data prop', () => {
-    expect(() => render(<TeachersList onSelect={() => {}} />)).toThrow();
-  });
-
-  it('should not render the component without onSelect prop', () => {
-    expect(() => render(<TeachersList data={[]} />)).toThrow();
-  });
-
   it('should render the item passed to the list', () => {
     const data: User[] = [
       {
@@ -26,7 +18,7 @@ describe('Teacher list', () => {
     ];
 
     const { getByText } = render(
-      <TeachersList data={data} onSelect={() => {}} />
+      <TeachersList data={data} onSelect={() => { }} />
     );
 
     const user = getByText('admin@gmail.com');
@@ -61,7 +53,7 @@ describe('Teacher list', () => {
 
   it('should not render the component without the props', () => {
     const { getByText } = render(
-      <TeachersList data={[]} onSelect={() => {}} />
+      <TeachersList data={[]} onSelect={() => { }} />
     );
 
     const emptyMsg = getByText('No data to display...');
