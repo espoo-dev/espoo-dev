0c185f6dcfc2664d58448f0178a2dd4a4021b160
Rocela Durazo <roxdurazo@gmail.com>
2021-10-20 17:49:19 -0700

    Feature: Create signup issue #373 Parse/convert signup "html page" to "slim page"
    - create signup slim file from html raw file

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   include Pundit
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role_id])
+  end
 end
