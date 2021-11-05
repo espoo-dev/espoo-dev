61631e334c7f405d588b45ecd0a71d1c40ebdfef
Edimo Silva <edimossilva@gmail.com>
2021-11-05 22:50:42 +0200

    Setup swagger

--- backend/spec/routes_spec.rb
+++ backend/spec/routes_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe 'Routes', type: :routing do
   end
 
   it 'has expected routes amount' do
-    expect(Rails.application.routes.routes.size).to eq(84)
+    expect(Rails.application.routes.routes.size).to eq(86)
   end
 end
