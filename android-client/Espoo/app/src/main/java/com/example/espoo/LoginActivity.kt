96dcbbaf9fa98963077e6da0c13464c7848a0d90
amiapmorais <amiaram91@gmail.com>
2021-11-08 12:08:33 -0300

    Creating login screen as a copy from https://espoolingo.herokuapp.com/login.
    Future fixes:
    -Change colors in night mode;
    -Remove ActionBar from login (if necessary);
    -Screen components needs accessibility attributes like contentDescription on imageView such that screen readers can adequately describe the user interface.
    -Layout doesn't allow scrolling.

--- android-client/Espoo/app/src/main/java/com/example/espoo/LoginActivity.kt
+++ android-client/Espoo/app/src/main/java/com/example/espoo/LoginActivity.kt
@@ -3,13 +3,10 @@ package com.example.espoo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
