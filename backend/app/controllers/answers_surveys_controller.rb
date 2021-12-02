d5b7a3448b2bfc0098c34818d139154dc2093216
garciajordy <jordygarcia0119@gmail.com>
2021-12-02 18:46:45 +0100

    Add question show endpoint
    When clicking on `Answer` on the surveys index
    page, the `AnswerSurveysController` creates and
    answer_survey but doesn't redirect to the first question.
    
    I add a redirect to a show endpoint in the
    `QuestionsController` in order to display the question correctly.

--- backend/app/controllers/answers_surveys_controller.rb
+++ backend/app/controllers/answers_surveys_controller.rb
@@ -1,9 +1,18 @@
 class AnswersSurveysController < ApplicationController
   def create
-    AnswersSurvey.create!(create_answers_survey_params)
+    @first_question = survey.questions.first
+    AnswersSurvey.find_or_create_by!(create_answers_survey_params)
+
+    redirect_to @first_question
+  rescue StandardError => err # rubocop:disable Naming/RescuedExceptionsVariableName
+    redirect_to surveys_path, flash: err.message
   end
 
   private
+
+  def survey
+    @survey ||= Survey.find(params[:survey_id])
+  end
 
   def create_answers_survey_params
     params.permit(:survey_id).merge(user_id: current_user.id)
