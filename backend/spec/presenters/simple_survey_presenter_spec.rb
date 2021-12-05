227b922275e3c8fb2f3b3d5a52aa46a55577257b
Edimo Silva <edimossilva@gmail.com>
2021-12-05 17:04:30 +0200

    Extract presenter to parent class

--- backend/spec/presenters/simple_survey_presenter_spec.rb
+++ backend/spec/presenters/simple_survey_presenter_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe SurveyPresenter do
+RSpec.describe SimpleSurveyPresenter do
   subject(:presenter) { described_class.new(survey, user) }
 
   let(:survey) { create(:survey_with_answer) }
@@ -14,20 +14,13 @@ RSpec.describe SurveyPresenter do
   it { expect(presenter).to respond_to(:payload).with(0).arguments }
 
   describe '#payload' do
-    it 'matches expected attributes when complete' do
-      answers_survey_payload = {
-        id: last_answers_survey.id,
-        status: last_answers_survey.status,
-        user_id: last_answers_survey.user_id
-      }
+    it 'matches expected attributes' do
       expected_payload = {
         id: survey.id,
         name: survey.name,
         description: survey.description,
-        total_questions_quantity: survey.questions.count,
-        answered_questions_quantity: survey.last_answers_quantity,
-        answers_surveys: [answers_survey_payload],
-        last_answers_survey: answers_survey_payload
+        total_questions_quantity: survey.questions.size,
+        answered_questions_quantity: survey.last_answers_quantity
       }
       expect(presenter.payload).to eq expected_payload
     end
