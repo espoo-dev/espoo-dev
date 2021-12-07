89179ad0a306f1283c0d938383c8b058ebb3625e
garciajordy <jordygarcia0119@gmail.com>
2021-12-07 09:59:07 +0100

    fixup! Add question show endpoint

--- backend/app/controllers/answers_surveys_controller.rb
+++ backend/app/controllers/answers_surveys_controller.rb
@@ -1,18 +1,14 @@
 class AnswersSurveysController < ApplicationController
   def create
-    @first_question = survey.questions.first
-    AnswersSurvey.create!(create_answers_survey_params)
+    answer_survey = AnswersSurvey.create!(create_answers_survey_params)
 
-    redirect_to @first_question
+    redirect_to answer_survey.survey.questions.first
   rescue StandardError => err # rubocop:disable Naming/RescuedExceptionsVariableName
-    redirect_to surveys_path, error: err.message
+    flash[:danger] = err.message
+    redirect_to surveys_path
   end
 
   private
-
-  def survey
-    @survey ||= Survey.find(params[:survey_id])
-  end
 
   def create_answers_survey_params
     params.permit(:survey_id).merge(user_id: current_user.id)
