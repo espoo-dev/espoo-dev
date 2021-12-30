aaf59ab09bf399d04c7052a612335bc5c9e3624c
Iury Nogueira <iunog@hotmail.com>
2021-12-30 13:43:44 -0300

    ✨ Validation to create answer survey (#470)
    * :test_tube: chore: add test case to survey not started #467
    
    * :sparkles: feat: validation to create new answer survey #467
    
    * :typo: fix typo #467

--- frontend/src/components/main/surveys-list.test.tsx
+++ frontend/src/components/main/surveys-list.test.tsx
@@ -12,6 +12,12 @@ jest.mock('../../api/client', () => ({
   },
 }));
 
+const mockResponse = {
+  mock: true,
+};
+
+const urlApiCreateAnswerSurvey = 'api/v1/answers_surveys';
+
 const surveyDefault: Survey = {
   id: 1,
   name: 'Animals survey',
@@ -49,10 +55,6 @@ describe('Surveys list', () => {
   });
 
   it('should call the function when clicked', async () => {
-    const mockResponse = {
-      mock: true,
-    };
-
     const surveyCompleted: Survey = { ...surveyDefault };
     surveyCompleted.current_answers_survey.status =
       AnswerSurveyStatus.Completed;
@@ -72,17 +74,13 @@ describe('Surveys list', () => {
     fireEvent.click(buttonItem);
 
     await waitFor(() =>
-      expect(httpClient.post).toHaveBeenCalledWith('api/v1/answers_surveys', {
+      expect(httpClient.post).toHaveBeenCalledWith(urlApiCreateAnswerSurvey, {
         survey_id: 1,
       })
     );
   });
 
   it('should not call api when select survey not started', async () => {
-    const mockResponse = {
-      mock: true,
-    };
-
     (httpClient as jest.Mocked<AxiosInstance>).post.mockImplementationOnce(
       jest.fn((url: string, body: AnswerSurveyCreate) => {
         return Promise.reject({ response: { data: mockResponse } });
@@ -90,8 +88,144 @@ describe('Surveys list', () => {
     );
 
     await waitFor(() =>
-      expect(httpClient.post).not.toHaveBeenCalledWith('api/v1/answers_surveys')
+      expect(httpClient.post).not.toHaveBeenCalledWith(urlApiCreateAnswerSurvey)
     );
     expect(screen.queryByText('Animals survey')).toBeNull();
+  });
+
+  it('should select survey when it is not started', async () => {
+    const mockSurveySelected = jest.fn();
+    const rendered = render(
+      <SurveysList setSurveySelected={mockSurveySelected} data={data} />
+    );
+    const buttonItem = rendered.getByTestId('Animals survey');
+
+    fireEvent.click(buttonItem);
+    (httpClient as jest.Mocked<AxiosInstance>).post.mockImplementationOnce(
+      jest.fn((url: string, body: AnswerSurveyCreate) => {
+        return Promise.reject({ response: { data: mockResponse } });
+      })
+    );
+
+    await waitFor(() =>
+      expect(httpClient.post).not.toHaveBeenCalledWith(urlApiCreateAnswerSurvey)
+    );
+  });
+
+  describe('Check create answer survey', () => {
+    it('should create answers survey when survey has no current answers survey', async () => {
+      const surveyWithoutCurrent: Survey = { ...surveyDefault };
+      surveyWithoutCurrent.current_answers_survey = null;
+      surveyWithoutCurrent.id = 2;
+
+      (httpClient as jest.Mocked<AxiosInstance>).post.mockImplementationOnce(
+        jest.fn((url: string, body: AnswerSurveyCreate) => {
+          return Promise.resolve({ data: mockResponse });
+        })
+      );
+
+      const { getByTestId } = render(
+        <SurveysList
+          setSurveySelected={jest.fn()}
+          data={[surveyWithoutCurrent]}
+        />
+      );
+
+      const buttonItem = getByTestId('Animals survey');
+
+      fireEvent.click(buttonItem);
+
+      await waitFor(() =>
+        expect(httpClient.post).toHaveBeenCalledWith(urlApiCreateAnswerSurvey, {
+          survey_id: 2,
+        })
+      );
+    });
+
+    it('should create answers survey when survey has current answers survey with status completed', async () => {
+      const surveyCompleted: Survey = { ...surveyDefault };
+      surveyCompleted.current_answers_survey.status =
+        AnswerSurveyStatus.Completed;
+      surveyCompleted.id = 3;
+
+      (httpClient as jest.Mocked<AxiosInstance>).post.mockImplementationOnce(
+        jest.fn((url: string, body: AnswerSurveyCreate) => {
+          return Promise.resolve({ data: mockResponse });
+        })
+      );
+
+      const { getByTestId } = render(
+        <SurveysList setSurveySelected={jest.fn()} data={[surveyCompleted]} />
+      );
+
+      const buttonItem = getByTestId('Animals survey');
+
+      fireEvent.click(buttonItem);
+
+      await waitFor(() =>
+        expect(httpClient.post).toHaveBeenCalledWith(urlApiCreateAnswerSurvey, {
+          survey_id: 3,
+        })
+      );
+    });
+
+    it('should not create answers survey when survey has current answers survey with status not started', async () => {
+      const surveyNotStarted: Survey = { ...surveyDefault };
+      surveyNotStarted.current_answers_survey.status =
+        AnswerSurveyStatus.NotStarted;
+      surveyNotStarted.id = 4;
+
+      (httpClient as jest.Mocked<AxiosInstance>).post.mockImplementationOnce(
+        jest.fn((url: string, body: AnswerSurveyCreate) => {
+          return Promise.resolve({ data: mockResponse });
+        })
+      );
+
+      const { getByTestId } = render(
+        <SurveysList setSurveySelected={jest.fn()} data={[surveyNotStarted]} />
+      );
+
+      const buttonItem = getByTestId('Animals survey');
+
+      fireEvent.click(buttonItem);
+
+      await waitFor(() =>
+        expect(httpClient.post).not.toHaveBeenCalledWith(
+          urlApiCreateAnswerSurvey,
+          {
+            survey_id: 4,
+          }
+        )
+      );
+    });
+
+    it('should not create answers survey when survey has current answers survey with status started', async () => {
+      const surveyStarted: Survey = { ...surveyDefault };
+      surveyStarted.current_answers_survey.status = AnswerSurveyStatus.Started;
+      surveyStarted.id = 5;
+
+      (httpClient as jest.Mocked<AxiosInstance>).post.mockImplementationOnce(
+        jest.fn((url: string, body: AnswerSurveyCreate) => {
+          return Promise.resolve({ data: mockResponse });
+        })
+      );
+
+      const { getByTestId } = render(
+        <SurveysList setSurveySelected={jest.fn()} data={[surveyStarted]} />
+      );
+
+      const buttonItem = getByTestId('Animals survey');
+
+      fireEvent.click(buttonItem);
+
+      await waitFor(() =>
+        expect(httpClient.post).not.toHaveBeenCalledWith(
+          urlApiCreateAnswerSurvey,
+          {
+            survey_id: 5,
+          }
+        )
+      );
+    });
   });
 });
