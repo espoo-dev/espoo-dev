28c3b74d8e94fbcbef6b6b80aa236baa8aa1b1f3
Edimo Silva <edimossilva@gmail.com>
2022-01-07 18:05:50 +0200

    Improve questions and surveys factories

---
+++ backend/spec/factories_spec/surveys_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Surveys' do
+  describe 'survey_with_2_questions' do
+    before do
+      create(:survey_with_2_questions)
+    end
+
+    it { expect(Survey.count).to eq(1) }
+
+    # TODO: this spec is not working
+    # it { expect(Question.count).to eq(2) }
+  end
+end
