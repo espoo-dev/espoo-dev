c44f9a42c51d1c86bacb7dcdb77cb60b9261c1bc
Edimo <edimossilva@gmail.com>
2022-01-22 18:56:07 +0200

    ignore auth token for login reques

--- backend/app/controllers/users/sessions_controller.rb
+++ backend/app/controllers/users/sessions_controller.rb
@@ -11,6 +11,7 @@ class Users::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   def create
+    request.headers['Authorization'] = nil
     self.resource = warden.authenticate!(auth_options)
     set_flash_message!(:notice, :signed_in) unless json_content_type_header?
 
