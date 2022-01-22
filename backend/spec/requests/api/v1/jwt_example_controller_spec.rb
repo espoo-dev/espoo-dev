f1d328c74749a04c9f97cbe8d73a6fdff67d8f48
Edimo <edimossilva@gmail.com>
2022-01-22 18:56:07 +0200

    ignore auth token for login reques

--- backend/spec/requests/api/v1/jwt_example_controller_spec.rb
+++ backend/spec/requests/api/v1/jwt_example_controller_spec.rb
@@ -1,3 +1,4 @@
+require 'rails_helper'
 require 'devise/jwt/test_helpers'
 
 describe JwtExampleController do
@@ -23,27 +24,52 @@ describe JwtExampleController do
     let(:user) { create(:user) }
 
     describe 'when data is valid' do
-      let(:params) do
-        { user: { 'email' => user.email, 'password' => user.password } }
+      describe 'when has no token' do
+        let(:params) do
+          { user: { 'email' => user.email, 'password' => user.password } }
+        end
+
+        before do
+          post '/users/sign_in', params: params, headers: headers
+        end
+
+        it { expect(response.status).to eq 200 }
+
+        it { expect(response.header['Authorization']).to start_with('Bearer') }
+
+        it 'return user data' do
+          expect(response_body).to include(
+            'id' => user.id,
+            'email' => user.email,
+            'role' => {
+              'id' => user.role.id,
+              'role_type' => user.role.role_type
+            }
+          )
+        end
       end
 
-      before do
-        post '/users/sign_in', params: params, headers: headers
-      end
+      describe 'when has token from another already user' do
+        let(:logged_user) { create(:user) }
 
-      it { expect(response.status).to eq 200 }
+        let(:params) do
+          { user: { 'email' => user.email, 'password' => user.password } }
+        end
 
-      it { expect(response.header['Authorization']).to start_with('Bearer') }
+        before do
+          post '/users/sign_in', params: params, headers: auth_headers(user: logged_user)
+        end
 
-      it 'return user data' do
-        expect(response_body).to include(
-          'id' => user.id,
-          'email' => user.email,
-          'role' => {
-            'id' => user.role.id,
-            'role_type' => user.role.role_type
-          }
-        )
+        it 'return user data' do
+          expect(response_body).to include(
+            'id' => user.id,
+            'email' => user.email,
+            'role' => {
+              'id' => user.role.id,
+              'role_type' => user.role.role_type
+            }
+          )
+        end
       end
     end
   end
