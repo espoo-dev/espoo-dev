5738deb45fa25c2dddeb9443b91cd195beec3066
PolinaStamenova <polina.stamenova@gmail.com>
2022-01-25 15:19:07 +0200

    Display type of questions in a Survey
    Add survey_helper.rb with methods:
    - `question_types` to create an array
    with the question types for a certain  survey.
    -'question_type_translation_key' to help the question types to be
    translated.

--- backend/spec/system/front_end/survey_crud_spec.rb
+++ backend/spec/system/front_end/survey_crud_spec.rb
@@ -25,6 +25,47 @@ RSpec.describe 'Survey CRUD', type: :system do
 
         it { expect(page).to have_text('2 questions answered') }
       end
+
+      context 'with single questions' do
+        let(:survey) { create(:ready_survey_single_choise_question, user: user_student) }
+
+        before do
+          create(:single_choice_ready_question, user: user_student)
+          visit surveys_path
+        end
+
+        it { expect(page).to have_text('Single choice type of questions.') }
+      end
+
+      context 'with multiple questions' do
+        before do
+          create(:multiple_choice_ready_question, user: user_student)
+          visit surveys_path
+        end
+
+        it { expect(page).to have_text('Multiple choice type of questions.') }
+      end
+
+      context 'with free text questions' do
+        let(:survey) { create(:ready_survey_free_text_question, user: user_student) }
+
+        before do
+          create(:free_text_ready_question, user: user_student)
+          visit surveys_path
+        end
+
+        it { expect(page).to have_text('Free text type of questions.') }
+      end
+
+      context 'with multiple choise and free text questions' do
+        let(:survey) { create(:ready_survey_with_diff_type_of_questions, user: user_student) }
+
+        before do
+          visit surveys_path
+        end
+
+        it { expect(page).to have_text('Multiple choice, Single choice type of questions.') }
+      end
     end
   end
 end
