501397bae1b400d17e33793af7fa35f927b458a7
PolinaStamenova <polina.stamenova@gmail.com>
2022-01-28 00:11:59 +0200

    Add create method in OptionsController for admin
    - The "administrate" gem redirect a user to the :show page after
    creating a new instance of an ActiveRecord. To redirect to the :index
    the create controller is configured.

--- backend/app/controllers/admin/options_controller.rb
+++ backend/app/controllers/admin/options_controller.rb
@@ -1,4 +1,17 @@
 module Admin
   class OptionsController < Admin::ApplicationController
+    def create
+      resource = resource_class.new(resource_params)
+      authorize_resource(resource)
+
+      if resource.save
+        flash[:notice] = t('administrate.option.created.success')
+        redirect_to admin_options_path
+      else
+        render :new, locals: {
+          page: Administrate::Page::Form.new(dashboard, resource)
+        }, status: :unprocessable_entity
+      end
+    end
   end
 end
