8922d8cde8b054f07bb9375fbfa394cb3737f73c
Iury Nogueira <iunog@hotmail.com>
2022-01-31 18:31:32 -0300

    :art: refactor multiple choise style and add new color to DS #583

--- frontend/src/components/multiple-choice/multiple-choice.styles.ts
+++ frontend/src/components/multiple-choice/multiple-choice.styles.ts
@@ -4,7 +4,7 @@ import * as transitions from 'styles/transitions';
 
 export const OptionMultipleChoice = styled.div<{ selected: boolean }>`
   background-color: ${(props) =>
-    (props.selected ? colorPallettes.bgSuccess : '#e4e8ee')};
+    (props.selected && colorPallettes.bgSuccess) || '#e4e8ee'};
   border-radius: 6px;
   cursor: pointer;
   padding: 12px;
