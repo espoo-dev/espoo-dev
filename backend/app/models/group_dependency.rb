e61cd162b74364e71bdecab941c7b80c33053de0
Edimo Silva <edimossilva@gmail.com>
2022-02-02 19:40:03 +0200

    Add groups to group dependency

--- backend/app/models/group_dependency.rb
+++ backend/app/models/group_dependency.rb
@@ -1,3 +1,4 @@
 class GroupDependency < ApplicationRecord
   belongs_to :group
+  has_many :groups, dependent: :nullify
 end
--- backend/spec/models/group_dependency_spec.rb
+++ backend/spec/models/group_dependency_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe GroupDependency, type: :model do
   describe 'relationships' do
     it { is_expected.to belong_to(:group).required }
-    # it { is_expected.to have_many(:groups).required }
+    it { is_expected.to have_many(:groups).dependent(:nullify) }
   end
 end
