4a0bdc051e19d8ba6b8a39fa48a658a995710a07
Edimo Silva <edimossilva@gmail.com>
2022-02-11 08:48:51 +0200

    Create endpoint to get trail by id

--- backend/spec/requests/api/v1/trails_request_spec.rb
+++ backend/spec/requests/api/v1/trails_request_spec.rb
@@ -29,4 +29,28 @@ RSpec.describe 'TrailsController', type: :request do
       it { expect(response).to have_http_status :ok }
     end
   end
+
+  describe '#show' do
+    let!(:trail) { create(:trail, groups: [create(:group_with_1_survey)], user: create(:user_student)) }
+    let!(:user) { trail.user }
+
+    context 'when trail exists' do
+      before { get api_v1_trail_path(trail), headers: auth_headers(user: user) }
+
+      it { expect(response).to have_http_status :ok }
+
+      it 'matches trail attributes' do
+        trail_payload = TrailPresenter.new(trail, user).payload.with_indifferent_access
+        expect(response_body).to match(trail_payload)
+      end
+    end
+
+    context 'when trail does not exist' do
+      before { get api_v1_trail_path(0), headers: auth_headers(user: user) }
+
+      it { expect(response).to have_http_status :not_found }
+
+      it { expect(response_body).to match({ 'error_message' => "Couldn't find Trail with [WHERE \"trails\".\"id\" = $1]" }) }
+    end
+  end
 end
