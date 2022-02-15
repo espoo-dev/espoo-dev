c6274de5dd369bc76010212725e5d9fe7ab39b68
Juan Landim <juan100mil@gmail.com>
2022-02-14 22:12:45 -0300

    implement timeout in testid roles

--- frontend/cypress/pages/RegisterPage.js
+++ frontend/cypress/pages/RegisterPage.js
@@ -2,10 +2,9 @@ class RegisterPage{
 
     register(register){
       cy.visit('/register')
-      cy.wait(2000)
       cy.get('[data-testid="email-register"]').type(register.email);
       cy.get('[data-testid="password-register"]').type(register.password);
-      cy.get(`[data-testid="${register.roles}"]`).click();
+      cy.get(`[data-testid="${register.roles}"]`,{ timeout: 10000 }).click();
       cy.get('[data-testid="register-btn"]').click();
       cy.get('div[class="Toastify__toast-body"]').should("have.text",register.alert_msg); 
     }
