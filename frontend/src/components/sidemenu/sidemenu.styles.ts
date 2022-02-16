09ecb824d093402b4e6a26c26e9f6881239d066f
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2022-02-16 19:14:39 -0300

    [feature/551] - improving survey list style

--- frontend/src/components/sidemenu/sidemenu.styles.ts
+++ frontend/src/components/sidemenu/sidemenu.styles.ts
@@ -1,12 +1,15 @@
+import { theme } from '@chakra-ui/react';
+import { colors } from '@styles/colors';
 import styled from 'styled-components';
-import { breakpoints } from 'styles/globals';
 import * as transitions from 'styles/transitions';
+
+const { breakpoints } = theme;
 
 export const MenuLinkOption = styled.a`
   padding: 15px 2em;
   display: flex;
   gap: 1em;
-  color: #fff;
+  color: ${colors.primaryTxt};
   cursor: pointer;
   text-decoration: none;
   border-radius: 12px;
@@ -17,41 +20,46 @@ export const MenuLinkOption = styled.a`
     transition: ${transitions.defaultTransition};
   }
 
-  @media (max-width: ${breakpoints.sm}) {
+  @media (max-width: ${breakpoints.md}) and (max-width: ${breakpoints.md}) {
     margin: 4px;
-    padding: 1em 1.5em;
+    padding: 10px;
+
+    &:first-child {
+      margin-left: 1em;
+    }
+  }
+
+  @media (max-width: ${breakpoints.sm}) {
     span {
       display: none;
     }
   }
 `;
 
-export const SideMenuContainer = styled.div`
+export const SideMenuContainer = styled.aside`
   display: flex;
   flex-direction: column;
   height: 100vh;
   width: 250px;
   padding: 2em;
   border-right: 1px solid;
+  border-radius: 0 30px 30px 0;
 
-  @media (max-width: ${breakpoints.sm}) {
-    height: auto;
-    width: auto;
+  @media (max-width: ${breakpoints.md}) {
+    border-radius: 0;
+    height: 80px;
+    width: 100%;
     padding: 1em 2em;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-
-    h1 {
-      font-size: 16px;
-    }
   }
 `;
 
 export const ItemMenu = styled.ul`
   margin: 40px 0px;
 
-  @media (max-width: ${breakpoints.sm}) {
+  @media (max-width: ${breakpoints.md}) {
     margin: 0px;
     display: flex;
   }
