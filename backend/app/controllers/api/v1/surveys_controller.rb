c0cb785861c4c91e90151834943b8f9a16e4470f
Edimo Silva <edimossilva@gmail.com>
2022-03-04 19:36:24 +0200

    Sort AnswersSurvey with sort method

--- backend/app/controllers/api/v1/surveys_controller.rb
+++ backend/app/controllers/api/v1/surveys_controller.rb
@@ -1,26 +1,20 @@
 class Api::V1::SurveysController < Api::V1::ApiController
-  include SurveyConcern
-
   def show
     survey = Survey.where(params.permit(:id)).includes(questions: %i[question_type options]).take!
     authorize survey
-    render json: parsed_surveys([survey]).first
+    render json: parsed_sorted_surveys([survey]).first
   end
 
   def index
     surveys = Survey.ready_surveys.includes(questions: %i[question_type options]).includes([:survey_subject])
     authorize surveys
 
-    render json: sorted_parsed_surveys(surveys)
+    render json: parsed_sorted_surveys(surveys)
   end
 
   private
 
-  def sorted_parsed_surveys(surveys)
-    sorted_surveys(surveys).map { |_status, array| parsed_surveys(array) }.flatten
-  end
-
-  def parsed_surveys(surveys)
-    surveys.map { |survey| SurveyPresenter.new(survey, current_user).payload }
+  def parsed_sorted_surveys(surveys)
+    surveys.map { |survey| SurveyPresenter.new(survey, current_user) }.sort(&:compare).map(&:payload)
   end
 end
