5ed0ce9a9945b0a2620604047226a13a377ccf0b
PolinaStamenova <polina.stamenova@gmail.com>
2022-03-07 10:48:32 +0200

    Show Groups on admin panel for user teacher

--- backend/app/policies/group_dependency_policy.rb
+++ backend/app/policies/group_dependency_policy.rb
@@ -1,2 +1,11 @@
-class GroupDependencyPolicy < ApplicationPolicy
+class GroupDependencyPolicy < GenericPolicy
+  class Scope < Scope
+    def search_params
+      groups_ids = user.groups.map do |group|
+        group.group_dependency&.id
+      end
+
+      { id: groups_ids }
+    end
+  end
 end
