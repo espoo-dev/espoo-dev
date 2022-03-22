343639e05a31e7a026bd130404bebcc636fbe8fc
PolinaStamenova <polina.stamenova@gmail.com>
2022-03-22 11:47:02 +0200

    Oveeride after_resource_created_path method
    This is done in order a user to be redirect to :new option page
    after creating a new option.

--- backend/app/controllers/admin/options_controller.rb
+++ backend/app/controllers/admin/options_controller.rb
@@ -5,13 +5,19 @@ module Admin
       authorize_resource(resource)
 
       if resource.save
-        flash[:notice] = t('administrate.option.created.success')
-        redirect_to admin_options_path
+        redirect_to(
+          after_resource_created_path(resource),
+          notice: t('administrate.option.created.success')
+        )
       else
         render :new, locals: {
           page: Administrate::Page::Form.new(dashboard, resource)
         }, status: :unprocessable_entity
       end
+    end
+
+    def after_resource_created_path(requested_resource)
+      [:new, namespace, requested_resource.class.name.downcase.to_sym]
     end
   end
 end
