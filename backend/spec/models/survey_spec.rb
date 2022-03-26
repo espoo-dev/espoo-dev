f2b9f35b2e3c560d765f05d2e338870f1f60ef03
PolinaStamenova <polina.stamenova@gmail.com>
2022-03-26 11:31:58 +0200

    Upgrade to rails 7

--- backend/spec/models/survey_spec.rb
+++ backend/spec/models/survey_spec.rb
@@ -57,47 +57,47 @@ RSpec.describe Survey, type: :model do
 
     it 'is valid when icon_url is empty' do
       survey.icon_url = nil
-      expect(survey.valid?).to eq true
+      expect(survey.valid?).to be true
     end
 
     it 'is valid when icon_url is an empty string' do
       survey.icon_url = ''
-      expect(survey.valid?).to eq true
+      expect(survey.valid?).to be true
     end
 
     it 'is valid when icon_url is url http' do
       survey.icon_url = 'http://www.google.com'
-      expect(survey.valid?).to eq true
+      expect(survey.valid?).to be true
     end
 
     it 'is valid when icon_url is url https' do
       survey.icon_url = 'https://www.facebook.com'
-      expect(survey.valid?).to eq true
+      expect(survey.valid?).to be true
     end
 
     it 'is valid when icon_url has prefix' do
       survey.icon_url = 'https://www.facebook.com/user/'
-      expect(survey.valid?).to eq true
+      expect(survey.valid?).to be true
     end
 
     it 'is invalid when icon_url does not have a protocol' do
       survey.icon_url = 'www.facebook.com'
-      expect(survey.valid?).to eq false
+      expect(survey.valid?).to be false
     end
 
     it 'is invalid when icon_url has error' do
       survey.icon_url = 'www.facebook. com'
-      expect(survey.valid?).to eq false
+      expect(survey.valid?).to be false
     end
 
     it 'is invalid when icon_url has a www error' do
       survey.icon_url = 'ww.facebook.com'
-      expect(survey.valid?).to eq false
+      expect(survey.valid?).to be false
     end
 
     it 'is invalid when icon_url do not have a second-level domain' do
       survey.icon_url = 'www.facebook'
-      expect(survey.valid?).to eq false
+      expect(survey.valid?).to be false
     end
   end
 
