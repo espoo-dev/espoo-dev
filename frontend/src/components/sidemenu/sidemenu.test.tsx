2eefe712cc5059fff0968d1f23065f62a72b3a8e
Iury Nogueira <iunog@hotmail.com>
2022-03-31 11:45:20 -0300

    ✨ Add redirect to home when logo is clicked #582 (#697)

--- frontend/src/components/sidemenu/sidemenu.test.tsx
+++ frontend/src/components/sidemenu/sidemenu.test.tsx
@@ -1,5 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { useRouter } from 'next/router';
 import { Sidemenu } from '.';
+
+jest.mock('../../api/client', () => ({
+  httpClient: {
+    post: jest.fn(),
+  },
+}));
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn()
+}));
+
+const mockRouter = {
+  replace: jest.fn()
+};
+(useRouter as jest.Mock).mockReturnValue(mockRouter);
 
 describe('Sidemenu', () => {
   it('should check system name', () => {
@@ -17,10 +33,17 @@ describe('Sidemenu', () => {
     expect(screen.getByText('Trails')).toBeInTheDocument();
   });
 
+  it('should redirect to main page when logo is clicked', async () => {
+    render(<Sidemenu />);
+    const logo = screen.getByTestId('logo-img');
+    fireEvent.click(logo);
+
+    expect(mockRouter.replace).toHaveBeenCalledWith('/surveys');
+  });
+
   it('should have a logout button', async () => {
     render(<Sidemenu />);
 
-    screen.debug();
     const logoutBtn = await screen.findByTestId('logout_button');
 
     expect(logoutBtn).toBeInTheDocument();
