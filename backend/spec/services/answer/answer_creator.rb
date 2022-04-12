aa03b09dc9e42280b655c298f6f363d841ad0b03
ValmirJuniorr <valmir_junyor@hotmail.com>
2022-04-12 11:52:58 -0300

    refactoring answer_creator rspec

--- backend/spec/services/answer/answer_creator.rb
+++ backend/spec/services/answer/answer_creator.rb
@@ -6,99 +6,61 @@ RSpec.describe Answer::AnswerCreator do
     let!(:user_student) { create(:user_student) }
     let!(:survey) { create(:survey, user: user_teacher) }
     let!(:question) { create(:multiple_choice_question, user: user_teacher, survey: survey) }
-    let!(:another_question) { create(:single_choice_question, user: user_teacher, survey: survey) }
     let!(:answers_survey) { create(:answers_survey, user: user_student, survey: survey) }
     let!(:option_a) { create(:option, user: user_teacher, question: question) }
     let!(:option_b) { create(:option, user: user_teacher, question: question) }
-    let!(:option_c) { create(:option, user: user_teacher, question: another_question) }
-    let!(:option_d) { create(:option, user: user_teacher, question: another_question) }
+
+    let(:question_id) { question.id }
+    let(:answers_survey_id) { answers_survey.id }
+    let(:answer_params) { { question_id: question_id, answers_survey_id: answers_survey_id } }
+    let(:option_ids) { [option_a.id, option_b.id] }
+    let(:user) { user_student }
+
+    subject(:creator_call) {
+      described_class.call(
+        answer_params: answer_params,
+        option_ids: option_ids,
+        user: user
+      )
+    }
 
     context 'when the params are correct' do
-      let(:option_ids) { [option_a.id, option_b.id] }
-      before do
-        Answer::AnswerCreator.call(
-          answer_params: { question_id: question.id, answers_survey_id: answers_survey.id },
-          option_ids: option_ids,
-          user: user_student
-        )
-      end
+      it { is_expected.to be_a Answer }
 
-      it { expect(Answer.count).to eq 1 }
+      it { expect { creator_call }.to change { Answer.count }.from(0).to(1) }
 
-      it { expect(Answer.first.question_id).to eq question.id }
+      it { expect(creator_call.answers_survey_id).to be answers_survey_id }
 
-      it { expect(Answer.first.option_ids).to eq option_ids }
+      it { expect(creator_call.question_id).to eq question_id }
 
-      context 'when already have answer created' do
-        before do
-          Answer::AnswerCreator.call(
-            answer_params: { question_id: another_question.id, answers_survey_id: answers_survey.id },
-            option_ids: [option_c.id],
-            user: user_student
-          )
-        end
-
-        it { expect(Answer.count).to eq 2 }
-
-        it { expect(Answer.second.question_id).to eq another_question.id }
-
-        it { expect(Answer.second.option_ids).to eq [option_c.id] }
-      end
+      it { expect(creator_call.option_ids).to match_array option_ids }
     end
 
-    context 'when the params are no correct' do
-      let(:question_id) { question.id }
-      let(:answers_survey_id) { answers_survey.id }
-      let(:answer_params) { { question_id: question_id, answers_survey_id: answers_survey_id } }
-      let(:option_ids) { [option_a.id, option_b.id] }
-      let(:user) { user_teacher }
+    context 'when the params are not correct' do
+      shared_examples 'raise error' do |nil_param_name, error|
+        context "when #{nil_param_name} is nil" do
+          it { expect { creator_call }.to raise_error error }
+        end
+      end
 
-      subject(:creator_call) {
-        described_class.call(
-          answer_params: answer_params,
-          option_ids: option_ids,
-          user: user
-        )
-      }
-
-      context 'when the user is nil' do
+      it_behaves_like 'raise error', 'user', Pundit::NotAuthorizedError do
         let(:user) { nil }
-
-        it 'raise Pundit::NotAuthorizedError' do
-          expect { creator_call }.to raise_error Pundit::NotAuthorizedError
-        end
       end
 
-      context 'when the option_ids is nil' do
+      it_behaves_like 'raise error', 'option_ids', ActiveRecord::RecordInvalid do
         let(:option_ids) { nil }
-
-        it 'raise ActiveRecord::RecordInvalid' do
-          expect { creator_call }.to raise_error ActiveRecord::RecordInvalid
-        end
       end
 
-      context 'when the answer_params is nil' do
+      it_behaves_like 'raise error', 'answer_params', ActiveRecord::RecordInvalid do
         let(:answer_params) { nil }
-
-        it 'raise ActiveRecord::RecordInvalid' do
-          expect { creator_call }.to raise_error ActiveRecord::RecordInvalid
-        end
       end
 
-      context 'when the question_id is nil' do
+      it_behaves_like 'raise error', 'question_id', ActiveRecord::RecordInvalid do
         let(:question_id) { nil }
-
-        it 'raise ActiveRecord::RecordInvalid' do
-          expect { creator_call }.to raise_error ActiveRecord::RecordInvalid
-        end
       end
 
-      context 'when the answers_survey_id is nil' do
+      it_behaves_like 'raise error', 'answers_survey_id', ActiveRecord::RecordInvalid do
         let(:answers_survey_id) { nil }
-
-        it 'raise ActiveRecord::RecordInvalid' do
-          expect { creator_call }.to raise_error ActiveRecord::RecordInvalid
-        end
       end
     end
   end
