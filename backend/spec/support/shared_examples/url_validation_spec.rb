0271be052f1961e4413207012f69174cd75a06f4
Philipe <philipe-leandro@hotmail.com>
2022-04-15 16:13:26 -0300

    Refactor url_validation.rb, icon_url and image_url fields tests

--- backend/spec/support/shared_examples/url_validation_spec.rb
+++ backend/spec/support/shared_examples/url_validation_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
-RSpec.shared_examples 'image_url validation' do
+RSpec.shared_examples 'url validation' do |field|
   it 'returns true' do
     ['', 'http://www.example.com', 'https://www.example.com', 'https://www.example.com/user'].each do |cases|
-      tested_class.image_url = cases
+      tested_class[field] = cases
+
       expect(tested_class.valid?).to be true
     end
   end
 
   it 'returns false' do
     ['www.example.com', 'www.example. com', 'ww.example.com', 'www.example'].each do |cases|
-      tested_class.image_url = cases
+      tested_class[field] = cases
+    
       expect(tested_class.valid?).to be false
     end
   end
