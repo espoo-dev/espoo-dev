1b86d2b60daae91aad9053a460c7bd44dedf6a06
Philipe <philipe-leandro@hotmail.com>
2022-04-15 17:42:31 -0300

    Change variable name in url_validation_spec.rb

--- backend/spec/support/shared_examples/url_validation_spec.rb
+++ backend/spec/support/shared_examples/url_validation_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.shared_examples 'url validation' do |field|
   it 'returns true' do
-    ['', 'http://www.example.com', 'https://www.example.com', 'https://www.example.com/user'].each do |cases|
-      object_instance[field] = cases
+    ['', 'http://www.example.com', 'https://www.example.com', 'https://www.example.com/user'].each do |url|
+      object_instance[field] = url
 
       expect(object_instance.valid?).to be true
     end
   end
 
   it 'returns false' do
-    ['www.example.com', 'www.example. com', 'ww.example.com', 'www.example'].each do |cases|
-      object_instance[field] = cases
+    ['www.example.com', 'www.example. com', 'ww.example.com', 'www.example'].each do |url|
+      object_instance[field] = url
 
       expect(object_instance.valid?).to be false
     end
