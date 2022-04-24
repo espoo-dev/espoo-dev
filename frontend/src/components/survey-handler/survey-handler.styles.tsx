4faad7c934b67423b0c4111f3fa6b5b53acecf60
Danilo Alves <dannalves111@gmail.com>
2022-04-24 14:40:57 -0300

    fix: responsiveness of question image

--- frontend/src/components/survey-handler/survey-handler.styles.tsx
+++ frontend/src/components/survey-handler/survey-handler.styles.tsx
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
 
-export const QuestionImage = styled.div<{ src: string }>`
+interface QuestionImageProps {
+  src: string;
+}
+
+export const QuestionImage = styled.div<QuestionImageProps>`
   margin-top: 0.75rem;
   border-radius: 6px;
-  width: 300px;
-  height: 300px;
+  max-width: 350px;
+  width: 60vw;
+  max-height: 300px;
+  height: 50vh;
   background-image: url(${(props) => props.src});
   background-size: cover;
   background-position: center;
